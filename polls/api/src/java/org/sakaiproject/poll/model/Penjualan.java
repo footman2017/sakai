5e3b20d39247b244129d5b7d773cb87ec1fce84e
Kiki Pratiwi <kiki.pratiwi.tif17@polban.ac.id>
2019-06-27 08:54:45 +0700

    ganti pojo

--- polls/api/src/java/org/sakaiproject/poll/model/Penjualan.java
+++ polls/api/src/java/org/sakaiproject/poll/model/Penjualan.java
@@ -13,11 +13,22 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
  */
 public class Penjualan {
     private long id;
-    private String Kd_Penjualan;
-    private String Kd_Customer;
-    private String Kd_Produk;
+    private int Kd_Penjualan;
+    private int Kd_Customer;
+    private int Kd_Produk;
     private int Jumlah_Barang;
     private long Total_Biaya;
+
+    public String toString() {
+        return new ToStringBuilder(this)
+        .append(this.getId())
+        .append(this.getKd_Penjualan())
+        .append(this.getKd_Customer())
+        .append(this.getKd_Produk())
+        .append(this.getJumlah_Barang())
+        .append(this.getTotal_Biaya())
+        .toString();
+    }
 
     /**
      * @return the id
@@ -36,42 +47,42 @@ public class Penjualan {
     /**
      * @return the Kd_Penjualan
      */
-    public String getKd_Penjualan() {
+    public int getKd_Penjualan() {
         return Kd_Penjualan;
     }
 
     /**
      * @param Kd_Penjualan the Kd_Penjualan to set
      */
-    public void setKd_Penjualan(String Kd_Penjualan) {
+    public void setKd_Penjualan(int Kd_Penjualan) {
         this.Kd_Penjualan = Kd_Penjualan;
     }
 
     /**
      * @return the Kd_Customer
      */
-    public String getKd_Customer() {
+    public int getKd_Customer() {
         return Kd_Customer;
     }
 
     /**
      * @param Kd_Customer the Kd_Customer to set
      */
-    public void setKd_Customer(String Kd_Customer) {
+    public void setKd_Customer(int Kd_Customer) {
         this.Kd_Customer = Kd_Customer;
     }
 
     /**
      * @return the Kd_Produk
      */
-    public String getKd_Produk() {
+    public int getKd_Produk() {
         return Kd_Produk;
     }
 
     /**
      * @param Kd_Produk the Kd_Produk to set
      */
-    public void setKd_Produk(String Kd_Produk) {
+    public void setKd_Produk(int Kd_Produk) {
         this.Kd_Produk = Kd_Produk;
     }
 
@@ -101,17 +112,6 @@ public class Penjualan {
      */
     public void setTotal_Biaya(long Total_Biaya) {
         this.Total_Biaya = Total_Biaya;
-    }
-    
-        public String toString() {
-        return new ToStringBuilder(this)
-        .append(this.id)
-        .append(this.Kd_Penjualan)
-        .append(this.Kd_Customer)
-        .append(this.Kd_Produk)
-        .append(this.Jumlah_Barang)
-        .append(this.Total_Biaya)
-        .toString();
     }
     
 }
